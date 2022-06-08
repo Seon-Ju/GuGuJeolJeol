4d3b63febd10a4f2977ef767e0cfd436db51c13e
Seon-Ju <o6o4_@naver.com>
2022-06-08 17:47:36 +0900

    FEAT: 검색 기능 추가

--- Gugujj/VC/SearchViewController.swift
+++ Gugujj/VC/SearchViewController.swift
@@ -14,12 +14,17 @@ class SearchViewController: BaseViewController {
     private let storage: Storage = Storage.storage()
     private let storagePath: String = "gs://gugujeoljeol-6f201.appspot.com/templeJSONData.json"
     
+    private var allTemples: [Temple] = [Temple]()
+    private var searchResultTemples: [Temple] = [Temple]()
     private var availableParkingTemples: [Temple] = [Temple]()
     private var availableCreditCardTemples: [Temple] = [Temple]()
     private var availablePetTemples: [Temple] = [Temple]()
     private var isHeritageTemples: [Temple] = [Temple]()
     
     private var isLoadedJSONData: Bool = false
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var searchTextField: UITextField!
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -36,13 +41,33 @@ class SearchViewController: BaseViewController {
         isSwipedFlag = false
         
         if !isLoadedJSONData {
-            downloadJSONData() { allTemples in
-                self.classifyTemplesByCategory(allTemples: allTemples)
+            downloadJSONData() {
+                self.classifyTemplesByCategory()
             }
         }
     }
     
     // MARK: - IBActions
+    @IBAction func touchUpSearchButton(_ sender: UIButton) {
+        searchResultTemples.removeAll()
+        
+        guard let searchText = searchTextField.text, searchText.count != 0 else {
+            showAlert(message: "검색어를 입력해주세요.")
+            return
+        }
+        
+        searchResultTemples = allTemples.filter { temple in
+            temple.title.contains(searchText)
+        }
+        
+        if !searchResultTemples.isEmpty {
+            SearchResultViewController.temples = searchResultTemples
+            CommonNavi.pushVC(sbName: "Main", vcName: "SearchResultVC")
+        } else {
+            showAlert(message: "검색결과가 없습니다.")
+        }
+    }
+    
     @IBAction func touchUpCategoryButton(_ sender: UIButton) {
         switch sender.restorationIdentifier {
         case "parkingButton":
@@ -60,36 +85,40 @@ class SearchViewController: BaseViewController {
     }
     
     // MARK: - Privates
-    private func downloadJSONData(completion: @escaping ([Temple]) -> Void) {
+    private func downloadJSONData(completion: @escaping () -> Void) {
         storage.reference(forURL: storagePath).downloadURL { url, error in
             let data = NSData(contentsOf: url!)! as Data
-            var allTemples = [Temple]()
             do {
-                allTemples = try JSONDecoder().decode([Temple].self, from: data)
-                completion(allTemples)
+                self.allTemples = try JSONDecoder().decode([Temple].self, from: data)
+                completion()
             } catch {
                 print("decodeError: \(error)")
             }
         }
     }
     
-    private func classifyTemplesByCategory(allTemples: [Temple]) {
-        for temple in allTemples {
-            if temple.parking == 1 {
-                availableParkingTemples.append(temple)
-            }
-            if temple.creditcard == 1 {
-                availableCreditCardTemples.append(temple)
-            }
-            if temple.pet == 1 {
-                availablePetTemples.append(temple)
-            }
-            if temple.heritage == 1 {
-                isHeritageTemples.append(temple)
-            }
+    private func classifyTemplesByCategory() {
+        availableParkingTemples = allTemples.filter { temple in
+            temple.parking == 1
+        }
+        availableCreditCardTemples = allTemples.filter { temple in
+            temple.creditcard == 1
+        }
+        availablePetTemples = allTemples.filter { temple in
+            temple.pet == 1
+        }
+        isHeritageTemples = allTemples.filter { temple in
+            temple.heritage == 1
         }
         isLoadedJSONData = true
         print("\(allTemples.count)개 로딩 및 분류 완료")
+    }
+    
+    private func showAlert(message: String) {
+        let action: UIAlertAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let alert: UIAlertController = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
